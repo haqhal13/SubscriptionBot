fc68fdfdc593a1e55fa2a547b94a0d3562d82e03
haqhal13 <haqhaleem06@gmail.com>
2025-01-09 21:43:26 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,5 @@
 # config.py
 
-# Telegram Bot Configuration
 BOT_TOKEN = "7559019704:AAEgnG14Nkm-x4_9K3m4HXSitCSrd2RdsaE"
-ADMIN_ID = 7618426591  # Replace with your Telegram admin ID
-GROUP_ID = -1002317604959  # Replace with your group ID
-
-# Webhook URLs
-WEBHOOK_REGISTER = "https://hook.eu2.make.com/hbcbusbsnzckbsnkhn2chuw42huqstfa"  # Webhook for invite
-WEBHOOK_MEMBER_JOIN = "https://hook.eu2.make.com/hbcbusbsnzckbsnkhn2chuw42huqstfa"  # Webhook for new member joins
-WEBHOOK_MONITOR = "https://hook.eu2.make.com/hbcbusbsnzckbsnkhn2chuw42huqstfa"  # Webhook for monitoring
+GROUP_ID = -1002317604959  # Group ID to track new members
+MAKE_WEBHOOK_URL = "https://hook.eu2.make.com/hbcbusbsnzckbsnkhn2chuw42huqstfa"  # Replace with your Make webhook URL
